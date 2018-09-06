37eb59a8c5ec2c09345857cff9fe7d0814313a43
shivangg <xshivangx@gmail.com>
2018-09-06 23:54:38 +0530

    feat: Publish RViz markers at pick and drop zone
    Published markers, waits, deletes marker, waits.

--- add_markers/src/basic_shapes.cpp
+++ add_markers/src/basic_shapes.cpp
@@ -7,10 +7,10 @@ int main( int argc, char** argv )
  ros::NodeHandle n;
  ros::Rate r(1);
  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
- 
+
      // Set our initial shape type to be a cube
  uint32_t shape = visualization_msgs::Marker::CUBE;
- 
+
  while (ros::ok())
  {
    visualization_msgs::Marker marker;
@@ -30,20 +30,20 @@ int main( int argc, char** argv )
    marker.action = visualization_msgs::Marker::ADD;
    
        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-   marker.pose.position.x = 0;
-   marker.pose.position.y = 0;
-   marker.pose.position.z = 0;
+   marker.pose.position.x = 4.43;
+   marker.pose.position.y = 6.46;
+   marker.pose.position.z =  0.0;
    marker.pose.orientation.x = 0.0;
    marker.pose.orientation.y = 0.0;
-   marker.pose.orientation.z = 0.0;
-   marker.pose.orientation.w = 1.0;
+   marker.pose.orientation.z = -0.70;
+   marker.pose.orientation.w = 0.71;
    
-       // Set the scale of the marker -- 1x1x1 here means 1m on a side
-   marker.scale.x = 1.0;
-   marker.scale.y = 1.0;
-   marker.scale.z = 1.0;
+   // Set the scale of the marker -- 1x1x1 here means 1m on a side
+   marker.scale.x = 0.50;
+   marker.scale.y = 0.50;
+   marker.scale.z = 0.50;
    
-       // Set the color -- be sure to set alpha to something non-zero!
+   // Set the color -- be sure to set alpha to something non-zero!
    marker.color.r = 0.0f;
    marker.color.g = 1.0f;
    marker.color.b = 0.0f;
@@ -51,7 +51,7 @@ int main( int argc, char** argv )
    
    marker.lifetime = ros::Duration();
    
-       // Publish the marker
+   // Publish the marker
    while (marker_pub.getNumSubscribers() < 1)
    {
      if (!ros::ok())
@@ -62,24 +62,62 @@ int main( int argc, char** argv )
      sleep(1);
    }
    marker_pub.publish(marker);
+
+   ROS_INFO("Marker published at pickup point");
+   sleep(5);
    
-       // Cycle between different shapes
-   switch (shape)
+   marker.action = visualization_msgs::Marker::DELETE;
+   marker_pub.publish(marker);
+   ROS_INFO("Marker deleted from pickup point");
+   sleep(5);
+
+   marker.action = visualization_msgs::Marker::ADD;
+   
+       // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+   marker.pose.position.x = -3.31;
+   marker.pose.position.y =  6.67;
+   marker.pose.position.z =  0.0;
+   marker.pose.orientation.x = 0.0;
+   marker.pose.orientation.y = 0.0;
+   marker.pose.orientation.z = -0.70;
+   marker.pose.orientation.w = 0.71;
+   
+   // Set the scale of the marker -- 1x1x1 here means 1m on a side
+   marker.scale.x = 0.50;
+   marker.scale.y = 0.50;
+   marker.scale.z = 0.50;
+   
+   // Set the color -- be sure to set alpha to something non-zero!
+   marker.color.r = 0.0f;
+   marker.color.g = 1.0f;
+   marker.color.b = 0.0f;
+   marker.color.a = 1.0;
+   
+   marker.lifetime = ros::Duration();
+   
+   // Publish the marker
+   while (marker_pub.getNumSubscribers() < 1)
    {
-     case visualization_msgs::Marker::CUBE:
-     shape = visualization_msgs::Marker::SPHERE;
-     break;
-     case visualization_msgs::Marker::SPHERE:
-     shape = visualization_msgs::Marker::ARROW;
-     break;
-     case visualization_msgs::Marker::ARROW:
-     shape = visualization_msgs::Marker::CYLINDER;
-     break;
-     case visualization_msgs::Marker::CYLINDER:
-     shape = visualization_msgs::Marker::CUBE;
-     break;
+     if (!ros::ok())
+     {
+       return 0;
+     }
+     ROS_WARN_ONCE("Please create a subscriber to the marker");
+     sleep(1);
    }
+   marker_pub.publish(marker);
+   ROS_INFO("Marker published at drop point");
+   sleep(5);
+
+   marker.action = visualization_msgs::Marker::DELETE;
+   marker_pub.publish(marker);
+   ROS_INFO("Marker deleted from drop point");
+   sleep(5);
+
+
    
+   
+
    r.sleep();
  }
 }
