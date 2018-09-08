59ebc0631378b95e2c60ba5454cb29efb2c59f7f
shivangg <xshivangx@gmail.com>
2018-09-08 18:51:14 +0530

    fix: Show marker with wait at pick drop zones

--- ShellScripts/add_markers.sh
+++ ShellScripts/add_markers.sh
@@ -1,8 +1,13 @@
 xterm -e 'roslaunch turtlebot_gazebo turtlebot_world.launch' &
 sleep 4
-xterm -e 'rosparam set objectAt "pick"' &
+xterm -e 'rosparam set objectAt "pick"' 
 xterm -e 'roslaunch turtlebot_rviz_launchers view_navigation.launch' &
 sleep 1
 xterm -e 'roslaunch turtlebot_gazebo amcl_demo.launch' &
 sleep 1
-xterm -e 'rosrun add_markers add_markers_node'
+xterm -e 'rosrun add_markers add_markers_node' &
+
+sleep 10
+xterm -e 'rosparam set objectAt ""' 
+sleep 5
+xterm -e 'rosparam set objectAt "drop"'
