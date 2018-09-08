6490c4f147061fcaaea997ba930ccd569f1b8885
shivangg <xshivangx@gmail.com>
2018-09-08 18:40:59 +0530

    feat: Add rosparam for add_marker node
    To put object visual marker in pickup zone

--- ShellScripts/add_markers.sh
+++ ShellScripts/add_markers.sh
@@ -1,7 +1,8 @@
 xterm -e 'roslaunch turtlebot_gazebo turtlebot_world.launch' &
 sleep 4
-xterm -e 'roslaunch turtlebot_gazebo amcl_demo.launch' &
-sleep 2
+xterm -e 'rosparam set objectAt "pick"' &
 xterm -e 'roslaunch turtlebot_rviz_launchers view_navigation.launch' &
+sleep 1
+xterm -e 'roslaunch turtlebot_gazebo amcl_demo.launch' &
 sleep 1
 xterm -e 'rosrun add_markers add_markers_node'
